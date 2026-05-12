407715ef74eb61d0e5d8709b1d6e5ed5f24de4be
Yanuar <yanuar.tht@gmail.com>
2026-05-13 05:23:09 +0700

    fix(i18n): revert ke original getRequestConfig

--- i18n/request.tsx
+++ i18n/request.tsx
@@ -1,10 +1,5 @@
 import { getRequestConfig } from 'next-intl/server';
-import { requestLocale } from 'next-intl/server';
 
-export default getRequestConfig(async () => {
-  const locale = await requestLocale();
-
-  return {
-    messages: (await import(`../messages/${locale}.json`)).default,
-  };
-});
+export default getRequestConfig(async ({ locale }) => ({
+  messages: (await import(`../messages/${locale}.json`)).default,
+}));
